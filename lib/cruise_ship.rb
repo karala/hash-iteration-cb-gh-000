55972943cdaf83f25d06267fb0257e526c188353
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-29 03:15:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,13 +5,13 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-winner = ""
 
 def select_winner(passengers)
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   end
+  winner
 end
-winner
